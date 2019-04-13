b695f92c03178446b8843dc7386c886dc4993a9c
Justin Leniger <jleniger@alarm.com>
2019-04-13 13:37:32 -0600

    fixed math and added driving

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -1,52 +1,33 @@
 <template>
   <div class="calculator">
-    <div class="field is-horizontal">
-      <div class="field-label is-large">
-        <label for="departing" class="label">Departing</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <div class="control is-large">
-            <input type="text" name="departing" class="input is-large" placeholder="Departing" v-model="departing">
-          </div>
-        </div>
+    <div class="field">
+      <label for="departing" class="label">Departing</label>
+      <div class="control is-large">
+        <input type="text" name="departing" class="input is-large" placeholder="Departing" v-model="departing">
       </div>
     </div>
-    <div class="field is-horizontal">
-      <div class="field-label is-large">
-        <label for="arriving" class="label">Arriving</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <div class="control is-large">
-            <input type="text" name="arriving" class="input is-large" placeholder="Arriving" v-model="arriving">
-          </div>
-        </div>
+    <div class="field">
+      <label for="arriving" class="label">Arriving</label>
+      <div class="control is-large">
+        <input type="text" name="arriving" class="input is-large" placeholder="Arriving" v-model="arriving">
       </div>
     </div>
-    <div class="field is-horizontal">
-      <div class="field-label">
-        <!-- Left empty for spacing -->
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <div class="control">
-            <button class="button is-primary" v-bind:class="{ 'is-loading': fetching, 'is-danger': error }" v-on:click="calculate">
-              Calculate
-            </button>
-          </div>
-        </div>
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <button class="button is-primary is-large" v-bind:class="{ 'is-loading': fetching, 'is-danger': error }" v-on:click="calculate">
+          Calculate
+        </button>
       </div>
     </div>
-    <section class="section" v-if="distance">
-      <h1 class="has-text-centered is-size-1">~{{ tonsOfCO2.toFixed(2) }} tons of CO<sub>2</sub></h1>
+    <section class="section" v-if="distanceKm">
+      <h1 class="has-text-centered is-size-1">about {{ tonsOfCO2.toFixed(2) }} tons of CO<sub>2</sub></h1>
+      <h2 class="has-text-centered is-size-4">or</h2>
+      <h1 class="has-text-centered is-size-1">
+        about {{ monthsDriven | pluralize('month') }} of driving<sup><a class="is-size-6" href="#citation-5"><cite>[5]</cite></a></sup><sup><a class="is-size-6" href="#citation-6"><cite>[6]</cite></a></sup><sup><a class="is-size-6" href="#citation-7"><cite>[7]</cite></a></sup>
+      </h1> 
     </section>
   </div>
 </template>
-
-<style>
-
-</style>
 
 <script>
 import axios from 'axios';
@@ -58,7 +39,7 @@ export default {
       departing: '',
       fetching: false,
       error: false,
-      distance: null
+      distanceKm: null
     }
   },
 
@@ -77,24 +58,60 @@ export default {
       if (res.status !== 200) {
         this.error = true;
       } else {
-        this.distance = res.data.result;
+        this.distanceKm = res.data.result;
       }
 
       this.fetching = false;
-    }
+    },
+
+    
   },
 
   computed: {
+    distanceMi: function () {
+      if (!this.distanceKm) {
+        return 0;
+      }
+
+      return this.distanceKm / 1.609;
+    },
+
+    roundtripDistanceMi: function () {
+      return 2 * this.distanceMi;
+    },
+
     lbsOfCO2: function() {
-      if (!this.distance) {
+      if (!this.distanceKm) {
         return null;
       }
 
-      return ( ((0.000134576 * Math.pow(this.distance, 2)) + (6.1798 * this.distance) + 3446.2 ) / 215.9 ) * 11.42224;
+      return 0.24 * this.roundtripDistanceMi;
+    },
+
+    monthsDriven: function() {
+      const AVG_MPG = 24.7;
+      const AVG_MILAGE = 13476;
+      const LBS_PER_GALLON = 18.07;
+
+      const GALLONS = AVG_MILAGE / AVG_MPG;
+
+      const LBS_PER_YEAR = LBS_PER_GALLON * GALLONS;
+
+      return Math.round((this.lbsOfCO2 / LBS_PER_YEAR) * 12);
     },
 
     tonsOfCO2: function() {
       return this.lbsOfCO2 / 2000;
+    }
+  },
+
+  filters: {
+    pluralize: function(value, word) {
+      if (value === 1) {
+        return `${value} ${word}`;
+      }
+
+      return `${value} ${word}s`;
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -9,13 +9,15 @@
     </div>
   </section>
   <section class="section">
-    <p class="has-text-centered">
+    <p>
       Business travel accounts for somewhere around 516 million passengers on flights every year.<sup><a href="#citation-1"><cite>[1]</cite></a></sup>
-      A roundtrip flight from NY to LA generates about 1.5 tons of CO<sub>2</sub> per passenger.<sup><a href="#citation-2"><cite>[2]</cite></a></sup>
-      This is nearly 774 million tons of CO<sub>2</sub> every year, just so HR can tell you your personality type.
+      A roundtrip flight from NY to LA generates about 0.6 tons of CO<sub>2</sub> per passenger, depending on how it's calculated.<sup><a href="#citation-2"><cite>[2]</cite></a></sup>
+      This is nearly 310 million tons of CO<sub>2</sub> every year (on the low end, compared to some sources<sup><a href="#citation-3"><cite>[3]</cite></a></sup>).
+      Just so HR can tell you your personality type.
     </p>
-    <p class="has-text-centered">
-      Let that sink in. The entire carbon output of Germany so you can shake a coworkers hand instead of seeing them on a screen.
+    <p>
+      Let that sink in. The entire carbon output of Poland<sup><a href="#citation-4"><cite>[4]</cite></a></sup>, roughly, so businessmen can shake a coworkers hand instead of seeing them on a screen.
+      This probably won't convince HR or your boss a phone call is better, but it's worth a shot.
     </p>
   </section>
   <section class="section">
@@ -23,7 +25,10 @@
     <calculator />
   </section>
   <section class="section">
-    <h4 class="has-text-centered is-size-4 is-italic has-text-weight-light">This site probably won't convince HR or your boss a phone call is better, but it's worth a shot.</h4>
+    <p class="has-text-weight-light is-size-6">
+      Dislaimer: I don't know what I'm doing and all the math could be wrong. I just care about climate change and don't like business trips.
+      It's pretty tricky to choose numbers for this type of thing as there are many studies and it's not clear which ones are more accurate.
+    </p>
   </section>
   <section class="section is-size-7">
     <ol>
@@ -32,7 +37,36 @@
         <p>Based on 4.3 billion passengers a year and a low estimate of 12% of passengers travelling for business.</p> 
       </li>
       <li id="citation-2">
+        <a href="https://blueskymodel.org/air-mile" target="_blank">BlueSkyModel</a>
+        <p>0.24 pounds per mile.</p>
+      </li>
+      <li id="citation-3">
         <a href="https://co2.myclimate.org" target="_blank">myclimate.org</a>
+        <p>1.5 tons rountrip NY to LA.</p>
+      </li>
+      <li id="citation-4">
+        <a href="https://ec.europa.eu/jrc/en/publication/eur-scientific-and-technical-research-reports/fossil-co2-emissions-all-world-countries-2018-report" target="_blank">
+          "Fossil CO2 emissions of all world countries - 2018 Report". Publications Office of the European Union.
+        </a>
+        <p>2017 Fossil CO<sub>2</sub> (MT CO<sub>2</sub>/yr).</p>
+      </li>
+      <li id="citation-5">
+        <a href="https://www.fhwa.dot.gov/ohim/onh00/bar8.htm" target="_blank">
+          U.S. Department of Transportation: Federal Highway Administration
+        </a>
+        <p>Average Annual Miles by Age Group.</p>
+      </li>
+      <li id="citation-6">
+        <a href="https://www.reuters.com/article/us-autos-emissions/u-s-vehicle-fuel-economy-rises-to-record-24-7-mpg-epa-idUSKBN1F02BX" target="_blank">
+          Reuters
+        </a>
+        <p>Average fuel efficiency of new cars, 2018.</p>
+      </li>
+      <li id="citation-7">
+        <a href="https://blueskymodel.org/gallon-gas" target="_blank">
+          BlueSkyModel
+        </a>
+        <p>18.07 lbs of CO<sub>2</sub> per gallon.</p>
       </li>
     </ol>
   </section>
@@ -50,6 +84,12 @@ export default {
 </script>
 
 <style>
+p {
+  margin-bottom: 18px;
+}
+li p {
+  margin-bottom: initial;
+}
 h4 {
   padding-bottom: 18px;
 }
